18fe52368f1dce8a3bc2c4873128484eea692195
RamiRafee <kingramy23@gmail.com>
2026-03-13 01:09:48 +0200

    finished: exercise1.7

--- part1/part1.d/unicafe/src/App.jsx
+++ part1/part1.d/unicafe/src/App.jsx
@@ -5,12 +5,19 @@ const Button = ({ onClick, text }) => {
 }
 
 const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+
   return (
     <div>
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
