27f6877795ac23637169867efae3524e99ecbcaa
RamiRafee <kingramy23@gmail.com>
2026-03-17 16:47:40 +0200

    finished:exercise 2.11

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -1,21 +1,24 @@
 
 import './App.css'
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    axios.get(baseUrl)
+    .then(res =>res.data)
+    .then(data => setPersons(data))
+  }, [])
   const handleSubmit = (event) => {
     event.preventDefault()
     if (persons.some(p => p.name.toLowerCase() === newName.toLowerCase())) {
